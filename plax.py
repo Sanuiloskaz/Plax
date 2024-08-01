fd216ec0be5324daa58d8b39841c5d4efbdead3a
sanuiloskaz <santidiazpinerez@gmail.com>
2024-08-01 15:28:30 -0500

    upgraded and ordered better

--- plax.py
+++ plax.py
@@ -1,100 +1,112 @@
 import google.generativeai as genai
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 from dotenv import load_dotenv
 import os
+import spotify_controls as sc
+
+
+
+functions_dictionary = {
+    'Spotify_Pause': sc.Function_Spotify_Pause,
+    'Spotify_Reproduce': sc.Function_Spotify_Reproduce,
+    'Spotify_Volume_Down': sc.Function_Spotify_Volume_Down,
+    'Spotify_Volume_Up': sc.Function_Spotify_Volume_Up,
+    'Spotify_Mute': sc.Function_Spotify_Mute,
+    'Spotify_Unmute': sc.Function_Spotify_Unmute,
+    'Spotify_Next_Song': sc.Function_Spotify_Next_Song,
+    'Spotify_Last_Song': sc.Function_Spotify_Last_Song,
+    'Spotify_Reproduce_Song': sc.Function_Spotify_Reproduce_Song,
+    'Spotify_Reproduce_Artist': sc.Function_Spotify_Reproduce_Artist,
+    'Spotify_Reproduce_Playlists': sc.Function_Spotify_Reproduce_Playlists
+}
+
+
+
+
+
+
 
 load_dotenv()
 
-scope = "user-read-playback-state,user-modify-playback-state"
-sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(client_id=os.getenv("CLIENT_ID"), client_secret=os.getenv("CLIENT_SECRET"), redirect_uri="https://callback.plax.app/",scope=scope))
+configuration_path = "configuration.txt"
+
+if os.path.exists(configuration_path):
+
+    with open(configuration_path, 'r', encoding='utf-8') as file:
+        file_content = file.read()
+
+else:
+    print("Error to find configuration file")
+
+first_config = file_content
 
 genai.configure(
     api_key = os.getenv("API_KEY")
 )
 
-start_volume = 100
-
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
 
-first_config = 'Gemini, si te preguntan como te llamas por favor di Plax, y si te saludan con, por ejemplo, Hola plax, necesito que reconozcas ese nombre. Si te pregunto sobre si algo es canción o artista solo responde "Canción" o "Artista"'
-sp.volume(start_volume)
-
 chat.send_message(first_config)
 
-def artist_or_song():
-    entry = question.strip()
-    aos = f"{entry} es canción o artista"
-    query = chat.send_message(aos)
-    if query.text == "Artista":
-        search_artist()
-    elif query.text == "Canción":
-        search_song()
+#def artist_or_song():
+    #entry = question.strip()
+#    aos = f"{entry} es canción o artista"
+ #   query = chat.send_message(aos)
+  #  if query.text == "Artista":
+    #    search_artist()
+   # elif query.text == "Canción":
+     #   search_song()
+    #else:
+      #  print("didn't found")
+
+#def search_artist():
+ #   text = question.strip()
+  #  word = "pon "
+   # position= text.find(word)
+   # query = text[position + len(word):]
+
+    #artist_id = sp.search(q=query, type="artist")["artists"]["items"][0]["id"]
+    #sp.start_playback(context_uri=f"spotify:artist:{artist_id}")
+
+#def search_song():
+#    text = question.strip()
+ #   word = "pon "
+  #  position= text.find(word)
+   # query = text[position + len(word):]
+
+    #results = sp.search(q=query, type="track")
+  #  song = results["tracks"]["items"][0]
+   # sp.start_playback(uris=[song["uri"]])
+
+#def search_playlist():
+ #   text = question.strip()
+  #  query = text.replace("playlist ", "")
+   # results = sp.search(q=query, type="playlist")
+#    playlist = results["playlists"]["items"][0]["uri"]
+ #   sp.start_playback(context_uri=playlist)
+
+def functions():
+    intro = response.text.replace("¤", "")
+    run_function = functions_dictionary.get(intro)
+    if run_function:
+        run_function()
     else:
-        print("didn't found")
+        print("error con eso pe")
 
-def search_artist():
-    text = question.strip()
-    word = "pon "
-    position= text.find(word)
-    query = text[position + len(word):]
-
-    artist_id = sp.search(q=query, type="artist")["artists"]["items"][0]["id"]
-    sp.start_playback(context_uri=f"spotify:artist:{artist_id}")
-
-def search_song():
-    text = question.strip()
-    word = "pon "
-    position= text.find(word)
-    query = text[position + len(word):]
-
-    results = sp.search(q=query, type="track")
-    song = results["tracks"]["items"][0]
-    sp.start_playback(uris=[song["uri"]])
 
 def chatbot():
+    global response
     response = chat.send_message(question)
     print('\n')
+    if "¤" in response.text:
+        functions()
+        
     print(f'Bot: {response.text}')
     print('\n')
 
-    
 
 while True:
     question = input("You: ")
-
-    if question.strip() == 'exit':
-        break
     
-    elif question.strip() == 'pausa':
-        sp.pause_playback()
-
-    elif question.strip() == 'reproduce':
-        sp.start_playback()
-
-    elif question.strip() == 'bajale':
-        if start_volume <= 0:
-            print("min vol")
-        else:
-            start_volume = start_volume-20
-            sp.volume(start_volume)
-    elif question.strip() == 'subele':
-        if start_volume >= 100:
-            print("max vol")
-        else:
-            start_volume = start_volume+20
-            sp.volume(start_volume)
-    elif question.strip() == 'silenciar':
-        sp.volume(0)
-    elif question.strip() == 'desmutea':
-        sp.volume(start_volume)
-    elif question.strip() == 'siguiente':
-        sp.next_track()
-    elif question.strip() == 'anterior':
-        sp.previous_track()
-    elif 'pon ' in question.strip():
-        artist_or_song()
-    else:
-        chatbot()
+    chatbot()
